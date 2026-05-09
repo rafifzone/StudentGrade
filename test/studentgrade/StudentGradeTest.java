dbdb2e5d9668887c717e2a40e2b407f943c8a684
User <User@DESKTOP-97BJEV5>
2026-05-09 04:50:59 +0300

    Reimplemented StudentGradeTest using JUnit 4

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,35 +1,40 @@
 package studentgrade;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
-    
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
+public class StudentGradeTest {
 
+    @Test
     public void testGetGrade95() {
-        System.out.println("getGrade 95");
+
         int mark = 95;
         String expResult = "A";
+
         String result = StudentGrade.getGrade(mark);
+
         assertEquals(expResult, result);
     }
 
+    @Test
     public void testGetGrade85() {
-        System.out.println("getGrade 85");
+
         int mark = 85;
         String expResult = "B";
+
         String result = StudentGrade.getGrade(mark);
+
         assertEquals(expResult, result);
     }
-    
+
+    @Test
     public void testGetGrade75() {
-        System.out.println("getGrade 85");
+
         int mark = 75;
         String expResult = "F";
+
         String result = StudentGrade.getGrade(mark);
+
         assertEquals(expResult, result);
     }
 }
